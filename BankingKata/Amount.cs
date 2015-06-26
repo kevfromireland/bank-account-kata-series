bd63cf7efd68c45121b27cffc5a9171bfc9bbe41
Kevin Boyle <kevin.boyle@outlook.com>
2015-06-26 14:29:04 +0100

    Amount is a thing

---
+++ BankingKata/Amount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BankingKata
+{
+    public class Amount : IEquatable<Amount>
+    {
+        private readonly uint m_Amount;
+
+        public Amount(uint amount)
+        {
+            m_Amount = amount;
+        }
+
+        public bool Equals(Amount other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return m_Amount == other.m_Amount;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Amount) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (int) m_Amount;
+        }
+    }
+}
---
+++ BankingKataTests/AmountTests.cs
@@ -0,0 +1,18 @@
+﻿using BankingKata;
+using NUnit.Framework;
+
+namespace BankingKataTests
+{
+    [TestFixture]
+    public class AmountTests
+    {
+        [Test]
+        public void TwoAmountsFromSameNumberAreEqual()
+        {
+            Amount amount1 = new Amount(1);
+            Amount amount2 = new Amount(1);
+
+            Assert.That(amount1, Is.EqualTo(amount2));
+        }
+    }
+}
