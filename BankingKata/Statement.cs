49b0ffc72c18b05523d1c9eef1806576d4915253
Kevin Boyle <kevin.boyle@outlook.com>
2015-06-29 15:24:14 +0100

    Print balance

---
+++ BankingKata/Statement.cs
@@ -0,0 +1,22 @@
+using System.IO;
+
+namespace BankingKata
+{
+    public class Statement
+    {
+        private readonly Account m_Account;
+        private readonly TextWriter m_StatementWriter;
+
+        public Statement(Account account, TextWriter statementWriter)
+        {
+            m_Account = account;
+            m_StatementWriter = statementWriter;
+        }
+
+        public void PrintBalance()
+        {
+            var balance = m_Account.CalculateBalance();
+            m_StatementWriter.WriteLine("Balance: £{0}", balance);
+        }
+    }
+}
---
+++ BankingKataTests/StatementTests.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Text;
+using BankingKata;
+using NUnit.Framework;
+
+namespace BankingKataTests
+{
+    [TestFixture]
+    public class StatementTests
+    {
+        [Test]
+        public void DefaultAccountsCanHaveBalancePrinted()
+        {
+            var resultBuffer = new StringBuilder();
+
+            var account = new Account(new Ledger());
+            new Statement(account, new StringWriter(resultBuffer)).PrintBalance();
+
+            Assert.That(resultBuffer.ToString(), Is.EqualTo("Balance: £0.00\r\n"));
+        }
+    }
+}
