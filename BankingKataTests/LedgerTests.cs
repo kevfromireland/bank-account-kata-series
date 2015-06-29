7171e668a3dccab4e592c28f3203393d459b4068
Kevin Boyle <kevin.boyle@outlook.com>
2015-06-29 15:31:53 +0100

    Finish this rename

--- BankingKataTests/LedgerTests.cs
+++ BankingKataTests/LedgerTests.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 namespace BankingKataTests
 {
     [TestFixture]
-    public class TransactionLogTests
+    public class LedgerTests
     {
         [Test]
         public void TotalIsInitiallyZero()
@@ -18,12 +18,12 @@ namespace BankingKataTests
         [Test]
         public void TotalEqualsSumOfAddedMoney()
         {
-            var transactionLog = new Ledger();
+            var ledger = new Ledger();
 
-            transactionLog.Record(new CreditEntry(new Money(1m)));
-            transactionLog.Record(new CreditEntry(new Money(3m)));
+            ledger.Record(new CreditEntry(new Money(1m)));
+            ledger.Record(new CreditEntry(new Money(3m)));
 
-            var actualTotal = transactionLog.Accept(new BalanceCalculatingVisitor(), new Money(0m));
+            var actualTotal = ledger.Accept(new BalanceCalculatingVisitor(), new Money(0m));
 
             var expectedTotal = new Money(4m);
             Assert.That(actualTotal, Is.EqualTo(expectedTotal));
@@ -32,12 +32,12 @@ namespace BankingKataTests
         [Test]
         public void TheTotalALogWithOnlyDebitsIsTheirNegativeSum()
         {
-            var transactionLog = new Ledger();
+            var ledger = new Ledger();
 
-            transactionLog.Record(new DebitEntry(new Money(1m)));
-            transactionLog.Record(new DebitEntry(new Money(3m)));
+            ledger.Record(new DebitEntry(new Money(1m)));
+            ledger.Record(new DebitEntry(new Money(3m)));
 
-            var actualTotal = transactionLog.Accept(new BalanceCalculatingVisitor(), new Money(0m));
+            var actualTotal = ledger.Accept(new BalanceCalculatingVisitor(), new Money(0m));
 
             var expectedTotal = new Money(-4m);
             Assert.That(actualTotal, Is.EqualTo(expectedTotal));
